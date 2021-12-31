2167f684c934efd21e1a56e9db1e67a209396263
fanjaekim <hjjang45@gmail.com>
2021-12-31 22:36:51 +0900

    C++ Socket Client - 단일 연결 Client 추가

---
+++ Tcp_Client/Tcp_Client.cpp
@@ -0,0 +1,39 @@
+﻿#include "stdafx.h"
+#include <iostream>
+#include "../ImaysNet/ImaysNet.h"
+#include <chrono>
+#include <thread>
+
+#pragma comment (lib, "./ImaysNet.lib")
+
+using namespace std;
+
+int main()
+{
+	try
+	{
+		Socket s(SocketType::Tcp); // 1
+		s.Bind(Endpoint::Any);     // 2
+		cout << "Connecting to server...\n";
+		s.Connect(Endpoint("127.0.0.1", 5959)); // 3
+
+		// 4
+		cout << "Sending data...\n";
+		const char* text = "hello";
+
+		// 보낼 문자열의 sz도 포함해서 전송해야 하므로 +1을 추가.
+		s.Send(text, strlen(text) + 1);
+
+		std::this_thread::sleep_for(1s);
+
+		// 5
+		cout << "Closing socket...\n";
+		s.Close();
+	}
+	catch (Exception& e)
+	{
+		cout << "Exception! " << e.what() << endl;
+	}
+	return 0;
+}
+
