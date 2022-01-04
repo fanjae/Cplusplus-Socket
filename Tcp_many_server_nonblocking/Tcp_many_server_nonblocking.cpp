08007e2e20be03b1ae46c25ad5239f1ce5a5b377
fanjaekim <hjjang45@gmail.com>
2022-01-04 17:29:44 +0900

    C++ Socket 여러 Client Non Blocking Ver - Tcp Server Non Blocking.cpp 내용 추가 - 기존 Ver과는 다르게 I/O 시에도 Blocking 되지 않음.

---
+++ Tcp_many_server_nonblocking/Tcp_many_server_nonblocking.cpp
@@ -0,0 +1,25 @@
+﻿#include "stdafx.h"
+#include "../ImaysNet/ImaysNet.h"
+#include <stdio.h>
+#include <signal.h>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <memory>
+#include <vector>
+#include <iostream>
+#include <mutex>
+
+using namespace std;
+
+// true가 되면 프로그램을 종료함.
+volatile bool stopWorking = false;
+
+void ProcessSignalAction(int sig_number)
+{
+	if (sig_number == SIGINT)
+	{
+		stopWorking = true;
+	}
+}
+
